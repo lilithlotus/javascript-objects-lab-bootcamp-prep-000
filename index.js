d628cf1807189eaec4d6f739d45225bf6634c3d6
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-07 06:07:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ function updateObjectWithKeyAndValue(object, key, value) {}
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {}
 
 function deleteFromObjectByKey(object, key) {}
+
+function destructivelyDeleteFromObjectByKey(object, key) {}
